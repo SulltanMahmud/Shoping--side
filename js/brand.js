18a285a1ca9ce9f87fab003801a3fa25ede62490
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-04-01 13:51:07 +0600

    first commit

---
+++ js/brand.js
@@ -0,0 +1,12 @@
+// Most popular brand
+
+const brandlogo = new Splide("#brand-logo", {
+  arrows: false,
+  pagination: false,
+  type: "loop",
+  perPage: 5,
+  autoplay: true,
+  interval: 4000,
+});
+
+brandlogo.mount();
